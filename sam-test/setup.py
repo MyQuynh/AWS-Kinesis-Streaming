100ef437b0bb41353afc1cb6a5ea5f650189698f
Quynh <Quynh@LAPTOP-P90IG20U>
2021-09-01 04:04:03 +0700

    Final

---
+++ sam-test/setup.py
@@ -0,0 +1,31 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        f = r':[a-z]+:`~?(.*?)`'
+        return re.sub(text_type(f), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="sam-kinesis-data-pipeline",
+    version="0.1.0",
+    url="https://github.com/MyQuynh/AWS-Kinesis-Streaming",
+    license='MIT',
+    description="This is a project example about AWS-Kinesis-Streaming",
+    long_description=read("README.md"),
+    packages=find_packages(exclude=('tests',)),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
